f826ee55fed08a1bc474ca7c99af805754be80d6
aj-hoang <ajmonkeydev@gmail.com>
2023-05-30 15:17:17 +0100

    Added spark dependency, added intiial import etl step

---
+++ data-source-land-registry/land-registry-etl/src/main/scala/com/landalytics/etl/landregistry/ImportRawToParquet.scala
@@ -0,0 +1,17 @@
+package com.landalytics.etl.landregistry
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import com.landalytics.model.landregistry.raw.RawLandRegistryModel.RawLandRegistry
+object ImportRawToParquet {
+  def run(spark: SparkSession, sourceUri: String, destinationUri: String, saveMode: SaveMode = SaveMode.Overwrite) = {
+    import spark.implicits._
+
+    spark.read
+      .option("header", "false")
+      .csv(sourceUri)
+      .as[RawLandRegistry]
+      .write
+      .mode(saveMode)
+      .parquet(destinationUri)
+  }
+}
