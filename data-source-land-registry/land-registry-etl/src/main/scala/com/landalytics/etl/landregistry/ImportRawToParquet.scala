cdadbc0d3ffbb602ddd41414513fe7f796dbcf3a
aj-hoang <ajmonkeydev@gmail.com>
2023-05-30 15:18:27 +0100

    Format import object

--- data-source-land-registry/land-registry-etl/src/main/scala/com/landalytics/etl/landregistry/ImportRawToParquet.scala
+++ data-source-land-registry/land-registry-etl/src/main/scala/com/landalytics/etl/landregistry/ImportRawToParquet.scala
@@ -2,6 +2,7 @@ package com.landalytics.etl.landregistry
 
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import com.landalytics.model.landregistry.raw.RawLandRegistryModel.RawLandRegistry
+
 object ImportRawToParquet {
   def run(spark: SparkSession, sourceUri: String, destinationUri: String, saveMode: SaveMode = SaveMode.Overwrite) = {
     import spark.implicits._
