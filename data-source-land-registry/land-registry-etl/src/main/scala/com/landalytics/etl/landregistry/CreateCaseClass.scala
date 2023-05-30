69d310c9cfd61343cc574358fdb89873c65448d1
aj-hoang <ajmonkeydev@gmail.com>
2023-05-30 17:35:06 +0100

    Added create case class, added and refactored tests

---
+++ data-source-land-registry/land-registry-etl/src/main/scala/com/landalytics/etl/landregistry/CreateCaseClass.scala
@@ -0,0 +1,47 @@
+package com.landalytics.etl.landregistry
+
+import com.landalytics.model.landregistry.raw.RawLandRegistryModel.RawLandRegistry
+import com.landalytics.model.landregistry.clean.CleanLandRegistryModel.LandRegistry
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+import java.sql.Date
+
+object CreateCaseClass {
+
+  private def convertLrDateOfTransferStringToDate(dateString: String): Date = {
+//    println(dateString.substring(0,10))
+    Date.valueOf(dateString.substring(0,10))
+  }
+
+  def run(spark: SparkSession, sourceUri: String, destinationUri: String, saveMode: SaveMode = SaveMode.Overwrite) = {
+    import spark.implicits._
+
+    val rawLrDS = spark.read.parquet(sourceUri).as[RawLandRegistry]
+
+    val lrDS = rawLrDS.map{ rawLr =>
+      LandRegistry(
+        transcationUniqueId = rawLr.transcationUniqueId,
+        price = rawLr.price.map(_.toDouble),
+        dateOfTransfer = rawLr.dateOfTransfer.map(convertLrDateOfTransferStringToDate),
+        postcode = rawLr.postcode,
+        propertyType = rawLr.propertyType,
+        oldNew = rawLr.oldNew,
+        duration = rawLr.duration,
+        saon = rawLr.saon,
+        paon = rawLr.paon,
+        street = rawLr.street,
+        locality = rawLr.locality,
+        townCity = rawLr.townCity,
+        district = rawLr.district,
+        county = rawLr.county,
+        ppdCategoryType = rawLr.ppdCategoryType,
+        recordStatus = rawLr.recordStatus
+      )
+    }
+
+    lrDS.write
+      .mode(saveMode)
+      .parquet(destinationUri)
+  }
+
+}
--- data-source-land-registry/land-registry-etl/src/test/scala/com/landalytics/etl/landregistry/CreateCaseClassTest.scala
+++ data-source-land-registry/land-registry-etl/src/test/scala/com/landalytics/etl/landregistry/CreateCaseClassTest.scala
@@ -4,25 +4,25 @@ import org.apache.spark.sql.SparkSession
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class ImportRawtoParquetTest extends AnyFlatSpec with Matchers {
-
+class CreateCaseClassTest extends AnyFlatSpec with Matchers {
   val spark = SparkSession.builder().master("local").getOrCreate()
   spark.sparkContext.setLogLevel("WARN")
 
-  it should "import raw to parquet" in {
-    val baseUri = "/ImportRawToParquet/land-registry"
-    val sourceUri = getClass.getResource(s"$baseUri/csv/land-registry.csv").getPath
-    val destinationUri = getClass.getResource(s"$baseUri").getPath ++ "/raw/land-registry.parquet"
+  it should "create the case class" in {
+    val baseUri = "/CreateCaseClass/land-registry"
+    val sourceUri = getClass.getResource(s"$baseUri/raw/land-registry.parquet").getPath
+    val destinationUri = getClass.getResource(s"$baseUri").getPath ++ "/cleanModel/land-registry.parquet"
     println(sourceUri)
     println(destinationUri)
 
-    // Should have no runtime errors
-    ImportRawToParquet.run(spark, sourceUri, destinationUri)
+    CreateCaseClass.run(spark, sourceUri, destinationUri)
 
     spark.read.parquet(destinationUri).printSchema()
     spark.read.parquet(destinationUri).show(false)
 
+    // should have no runtime errors
     succeed
+
   }
 
 }
