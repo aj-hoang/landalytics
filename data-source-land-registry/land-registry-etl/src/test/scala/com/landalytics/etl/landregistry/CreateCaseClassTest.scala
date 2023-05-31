57bd58e173a1dcdc4d272e784e35f6818a86adb3
aj-hoang <ajmonkeydev@gmail.com>
2023-05-31 11:34:45 +0100

    Removed test parquet file, adjusted test to dynamically create one during the test utilizing the raw case class

--- data-source-land-registry/land-registry-etl/src/test/scala/com/landalytics/etl/landregistry/CreateCaseClassTest.scala
+++ data-source-land-registry/land-registry-etl/src/test/scala/com/landalytics/etl/landregistry/CreateCaseClassTest.scala
@@ -1,17 +1,46 @@
 package com.landalytics.etl.landregistry
 
-import org.apache.spark.sql.SparkSession
+import com.landalytics.model.landregistry.raw.RawLandRegistryModel.RawLandRegistry
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CreateCaseClassTest extends AnyFlatSpec with Matchers {
   val spark = SparkSession.builder().master("local").getOrCreate()
   spark.sparkContext.setLogLevel("WARN")
+  import spark.implicits._
 
   it should "create the case class" in {
-    val baseUri = "/CreateCaseClass/land-registry"
-    val sourceUri = getClass.getResource(s"$baseUri/raw/land-registry.parquet").getPath
-    val destinationUri = getClass.getResource(s"$baseUri").getPath ++ "/cleanModel/land-registry.parquet"
+
+    // Write sample RawLandRegistry to parquet
+    val sampleRawDS = List(RawLandRegistry(
+      transactionUniqueId = "someId",
+      price = Some("12345"),
+      dateOfTransfer = Some("2023-01-01 00:00"),
+      postcode = Some("SomePostcode"),
+      propertyType = None,
+      oldNew = None,
+      duration = None,
+      paon = None,
+      saon = None,
+      street = None,
+      locality = None,
+      townCity = None,
+      district = None,
+      county = None,
+      ppdCategoryType = None,
+      recordStatus = None
+    )).toDS
+
+    val baseUri = getClass.getResource("/CreateCaseClass/land-registry").getPath
+    val sampleRawLandRegistryUri = baseUri ++ "/raw/land-registry.parquet"
+    sampleRawDS.write
+      .mode(SaveMode.Overwrite)
+      .parquet(sampleRawLandRegistryUri)
+
+    // Use above created parquet in createCaseClass call
+    val sourceUri = sampleRawLandRegistryUri
+    val destinationUri = baseUri ++ "/cleanModel/land-registry.parquet"
     println(sourceUri)
     println(destinationUri)
 
