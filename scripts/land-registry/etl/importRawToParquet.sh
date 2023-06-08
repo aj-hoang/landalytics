79fb613dfd1cc1712cbd71761d882133ca616c5c
aj-hoang <ajmonkeydev@gmail.com>
2023-06-08 14:46:41 +0100

    Added example spark-submit script for land-registry etl

--- scripts/land-registry/etl/importRawToParquet.sh
+++ scripts/land-registry/etl/importRawToParquet.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+spark-submit \
+  --class com.landalytics.etl.landregistry.ImportRawToParquet \
+  --master local[*] \
+  /home/anthony/temp/land-registry-etl-shadow_2.12.jar \
+  -s <source-uri> \
+  -d <destination-uri>
