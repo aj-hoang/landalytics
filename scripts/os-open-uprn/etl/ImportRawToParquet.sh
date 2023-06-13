81037f3cfe2ec4e5f083c2f69d3ce854b1d87f2a
aj-hoang <ajmonkeydev@gmail.com>
2023-06-13 10:25:49 +0100

    Updated build.gradle to shade certain libraries so they do not conflict with spark's libraries on its classpath on a spark cluster, added run script for os-open-uprn etl, added initial config

---
+++ scripts/os-open-uprn/etl/ImportRawToParquet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PROJECT_ROOT=
+DATASOURCE=os-open-uprn
+SOURCE_CONFIG_LOCATION=$PROJECT_ROOT/scripts/$DATASOURCE/config/os-open-uprn.conf
+RUN_CONFIG_DIRECTORY=$PROJECT_ROOT/run/etl/$DATASOURCE/
+RUN_CONFIG_LOCATION=$RUN_CONFIG_DIRECTORY/os-open-uprn.conf
+
+# Copy config to "run" location
+mkdir -p $RUN_CONFIG_DIRECTORY
+
+# Replace @PROJECTROOT@ with variable
+sed "s#@PROJECTROOT@#$PROJECT_ROOT#g" $SOURCE_CONFIG_LOCATION > $RUN_CONFIG_LOCATION
+
+spark-submit \
+  --class com.landalytics.etl.osopenuprn.ImportRawToParquet \
+  --master local[*] \
+  $PROJECT_ROOT/data-source-os-open-uprn/os-open-uprn-etl/build/libs/os-open-uprn-etl-shadow_2.12.jar \
+  -c $RUN_CONFIG_LOCATION
+
