5caab990e3fde5de8159dfe4771a2edc71fd8c94
aj-hoang <ajmonkeydev@gmail.com>
2023-06-14 10:45:04 +0100

    Added script for running GenerateAddressCore, added related conf files, adjusted spark-submmit parameters for os-open-uprn etl scripts, renamed os open uprn output locations

--- scripts/address-core/etl/GenerateAddressCore.sh
+++ scripts/address-core/etl/GenerateAddressCore.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 PROJECT_ROOT=
-DATASOURCE=os-open-uprn
-SOURCE_CONFIG_LOCATION=$PROJECT_ROOT/scripts/$DATASOURCE/config/os-open-uprn.conf
+DATASOURCE=address-core
+SOURCE_CONFIG_LOCATION=$PROJECT_ROOT/scripts/$DATASOURCE/config/address-core.conf
 RUN_CONFIG_DIRECTORY=$PROJECT_ROOT/run/etl/$DATASOURCE/
-RUN_CONFIG_LOCATION=$RUN_CONFIG_DIRECTORY/os-open-uprn.conf
+RUN_CONFIG_LOCATION=$RUN_CONFIG_DIRECTORY/address-core.conf
 
-if  [ -z $PROJECT_ROOT ]
+
+if [[ -z $PROJECT_ROOT ]]
 then
   echo "PROJECT_ROOT is empty, skipping..."
 else
@@ -16,12 +17,12 @@ else
   sed "s#@PROJECTROOT@#$PROJECT_ROOT#g" $SOURCE_CONFIG_LOCATION > $RUN_CONFIG_LOCATION
 
   spark-submit \
-    --class com.landalytics.etl.osopenuprn.CreateCaseClass \
+    --class com.landalytics.etl.addresscore.GenerateAddressCore \
     --master local[*] \
     --executor-memory 2G \
     --driver-memory 2G \
     --num-executors 4 \
-    $PROJECT_ROOT/data-source-os-open-uprn/os-open-uprn-etl/build/libs/os-open-uprn-etl-shadow_2.12.jar \
+    $PROJECT_ROOT/landalytics-address-core/address-core-etl/build/libs/address-core-etl-shadow_2.12.jar \
     -c $RUN_CONFIG_LOCATION
 
 fi
